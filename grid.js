2ba77c6c6647cd93f52789f9d8dd7e2fe692f060
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-10-07 14:21:26 +0300

    Grid rendered
    Proceed with brushing and deleting

--- grid.js
+++ grid.js
@@ -1,18 +1,44 @@
 import { Hexagon } from "./hexagon.js";
 
-export class Grid{
+export class Grid{ //flat top even
     gridLength;
     grid = []; //2d array of hexagons
 
     constructor(gridLength){
         this.gridLength = gridLength;
     }
-    initGrid(gl){ //inits an empty grid
+    /**
+     * 
+     * @param {*} firstTopRight top right vert pos of the first hexagon (0, 0), in world space coordinates
+     */
+    initGrid(firstTopRight){ //inits an empty grid
         this.grid = [];
+ 
 
         for(let i = 0; i < this.gridLength; i++){
+            this.grid.push([]);
             for(let j = 0; j < this.gridLength; j++){
-                
+                const currX = firstTopRight.x + j * (1.5 * Hexagon.WORLD_SIDE_LENGTH);
+                const currY = firstTopRight.y + i * (Math.sqrt(3) * Hexagon.WORLD_SIDE_LENGTH) - ((j % 2) * Hexagon.WORLD_SIDE_LENGTH / (Math.sqrt(3) / 2.0));
+
+                let currTopRight = {x: currX, y: currY};
+                let currHex = new Hexagon(currTopRight);
+
+                this.grid[i][j] = currHex;
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param {*} gl 
+     * Programmatically renders each hexagon to comprise a total grid
+     */
+    renderGrid(gl){
+        for(let i = 0; i < this.gridLength; i++){
+            for(let j = 0; j < this.gridLength; j++){
+                let currHex = this.grid[i][j];
+                currHex.render(gl);
             }
         }
     }
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import { Hexagon } from "./hexagon.js";
+import { Grid } from "./grid.js";
 
 let canvas = document.getElementById('canvas');
 let gl = canvas.getContext('webgl2');
@@ -9,5 +10,13 @@ console.log("Debug: canvas width and height are", canvas.width, canvas.height);
 Hexagon.initProgram(gl, canvas.width, canvas.height);
 console.log("Debug: Hexagon regular vertPos is ", Hexagon.VERT_POS);
 
-let hex = new Hexagon({x: 30, y: 30});
-hex.render(gl);
+//let hex = new Hexagon({x: 30, y: 30});
+//hex.render(gl);
+
+console.log("Debug: about to initialize grid then render a whole grid");
+let n = 50;
+
+let firstTopRight = {x: 30, y: 30};
+let grid = new Grid(n);
+grid.initGrid(firstTopRight);
+grid.renderGrid(gl);
